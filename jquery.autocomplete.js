bb4c8c0299092700f6e77c5f51a23ae306207df3
Richard Sussex <info@designbysilverside.co.uk>
2011-11-23 22:50:54 +0000

    Update comments in jquery.autocomplete.js . Created jquery.autocomplete.min.js using http://jscompress.com/.

--- jquery.autocomplete.js
+++ jquery.autocomplete.js
@@ -7,44 +7,34 @@
 
 (function( $ ){
 	
-		
   $.fn.autoComplete = function( options ) {
 
-	// the fields name
+	// the element name
 	var fieldName = $(this).attr('name');
 
-	// add a class to this field 
+	// add a class to this element 
 	$(this).addClass('autoComplete-active');
 	  
-	// remove this the name of this field and create a hidden field with the same name
+	// remove the name of this field
 	$(this).attr('name', null);
 
+	// create a hidden field with the same name
 	var hiddenField = '<input type="hidden" class="autoComplete-hiddenField" name="'+fieldName+'" />';
 	 
-	// Create some defaults, extending them with any options that were provided
+	// create defaults, extend them with any options that were provided
     var settings = $.extend( {
       'url'           : null,
       'minchars'	  : 4
     }, options);
 
-	// wrap the field
+	// wrap this element
 	$(this).wrap('<div class="autoComplete-holder">');
 
-	// this parent wrapper
+	// reference the parent wrapper
 	var parentWrapper = $(this).parent('.autoComplete-holder');
 
 	// add the hidden field
 	$(parentWrapper).prepend(hiddenField);
-
-	// position
-	var position = this.position();
-	
-	// var top
-	var top = position.top;
-	top = (top + $(this).outerHeight());
-	
-	// var left
-	var left = position.left;
 
 	// on key up
     $(this).keyup(function(event) {
@@ -52,55 +42,74 @@
     	// keys not triggered
     	var ignoredKeys = [8, 13, 16, 17, 18, 19, 20, 27, 32, 33, 34, 35, 37, 38, 39, 40, 36, 45, 46, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123];
     
-    	// if event key code not shift
+    	// if event key code not in ignoredKeys are
     	if($.inArray(event.keyCode, ignoredKeys) == -1) {
     	
-			// remove existing
+			// remove existing .list-holder
 			$('.list-holder', parentWrapper).remove();
 		    
-			// current value
+			// the value of this element
 			var currentValue = $(this).val();
 	
-			// store a reference to this
-			var thisElement = this;
+			// store a reference to this for use inside $.ajax
+			var e = this;
 			
-		    // if the length is greater than or equal to 4
+		    // if the length is greater than or equal to settings.minchars
 			if(currentValue.length >= settings.minchars) {
 
-				// get the data
+				// get the data with $.ajax
 				$.ajax({
-					url : settings.url,
-					dataType : 'json',
-					data : 'searchTerm='+currentValue,
+					url : settings.url, // url
+					dataType : 'json',  // json dataType
+					data : 'searchTerm='+currentValue, // send the serach term
 					success : function(data) {
+						
 						// remove the list holder
 						$('.list-holder', parentWrapper).remove(); 
-						// var html
+						
+						// html var
 						var html = '';
 						
-						// data
+						// loop through the data and create li fields
 						for(var i in data) {
 							var html = html + '<li id="'+data[i].id+'">'+data[i].label+'</li>';
 						}
 						
-						// append the list holder
-						if($(thisElement).css('position') == 'absolute') {
-							$(parentWrapper).append('<div class="list-holder" style="position:absolute;left:'+left+'px;top:'+top+'px;width:'+($(thisElement).outerWidth()-2)+'px;"><ul>'+html+'</ul></div>');
+						// check the position of e and append accordingly
+						if($(e).css('position') == 'absolute') {
+
+							// position of this element
+							var position = $(e).position();
+							
+							// top of this element
+							var top = position.top;
+							
+							// add the outer height of this element to the top value 
+							top = (top + $(e).outerHeight());
+							
+							// left of this element
+							var left = position.left;
+							
+							// append the list-holder
+							$(parentWrapper).append('<div class="list-holder" style="position:absolute;left:'+left+'px;top:'+top+'px;width:'+($(e).outerWidth()-2)+'px;"><ul>'+html+'</ul></div>');
+						
 						} else {
-							$(parentWrapper).append('<div class="list-holder" style="position:absolute;width:'+($(thisElement).outerWidth()-2)+'px;"><ul>'+html+'</ul></div>');
+							// append the list-holder
+							$(parentWrapper).append('<div class="list-holder" style="position:absolute;width:'+($(e).outerWidth()-2)+'px;"><ul>'+html+'</ul></div>');
 						}
 						
-						// add a click function to the list-holder
+						// add a click function to the list-holder li's (the search terms)
 						$('ul li', parentWrapper).click(function() {
 							
-							// put the id in the hidden field
+							// put the id of the selection in the hidden field
 							$('.autoComplete-hiddenField', parentWrapper).val($(this).attr('id'));
 							
-							// put the value in the search field
-							$(thisElement).val($(this).text());
+							// put the value of the selection in the search field
+							$(e).val($(this).text());
 	
-							// remove existing
+							// slideUp the list-holder
 							$('.list-holder', parentWrapper).slideUp(400,function() {
+								// remove on list-holder from the DOM
 								$('.list-holder', parentWrapper).remove(); 
 							});
 							
